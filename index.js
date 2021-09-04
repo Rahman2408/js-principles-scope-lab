0010ec481997e1332a1d7f02725b5a6329c478ba
Rahman Elsirafy <rahman2408@gmail.com>
2021-09-04 19:44:40 -0400

    commit tree fe82ad9ad52a95e294be67ee9bbd4a05e023a18f

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName;
+customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+  return customerName;
+};
+
+
+
+var bestCustomer; 
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+};
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+};
+
+const leastFavoriteCustome = 'Alan';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustome = 'error';
+
+};
